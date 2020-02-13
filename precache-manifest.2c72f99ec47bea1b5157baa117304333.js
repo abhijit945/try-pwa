e251b567c78dbe99e7b21fc1dc1b40865b58af9d
Abhijit Rao <abhijit945@gmail.com>
2020-02-12 19:04:07 -0600

    Updates

---
+++ precache-manifest.2c72f99ec47bea1b5157baa117304333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae800120262359b56670db8663fdd4a1",
+    "url": "/try-pwa/index.html"
+  },
+  {
+    "revision": "d941ab8e68974c379562",
+    "url": "/try-pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "36b1c41a81c3f01e52ca",
+    "url": "/try-pwa/static/js/2.9bf5d9e0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/try-pwa/static/js/2.9bf5d9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d941ab8e68974c379562",
+    "url": "/try-pwa/static/js/main.535ed272.chunk.js"
+  },
+  {
+    "revision": "8c3c7f15890b7a9a1296",
+    "url": "/try-pwa/static/js/runtime-main.8f7742aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/try-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
